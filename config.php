1ad1d3de8025f9b3e2b8d88a290f3712acd0a0f2
IsaquiasMarques <patisaquias2000@gmail.com>
2023-05-25 00:26:28 +0100

    api deployment

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'mailer' => [
+        'clientName' => 'Orlando Residencial',
+        'clientUserEmail' => 'comercial@broima.co.ao',
+        'clientUserPassword' => 'BROIMAc12345!',
+        'mailHost' => 'mail.broima.co.ao',
+        'mailPort' => 587,
+        'mailCharset' => 'UTF-8'
+    ]
+];
