686bfa064b8e66eed10cb0f7c771dfcbbe28a353
IsaquiasMarques <patisaquias2000@gmail.com>
2023-06-14 22:42:03 +0100

    opening pictures added gallery and typology improved

--- src/app/core/data/api/mock/mock-categories.ts
+++ src/app/core/data/api/mock/mock-categories.ts
@@ -1,27 +1,32 @@
 export const CATEGORIES: any[] = [
     {
       id: 1,
-      name: 'Tipologia T3',
+      name: 'Abertura',
       active: false,
     },
     {
       id: 2,
-      name: 'Tipologia T4A',
+      name: 'Tipologia T3',
       active: false,
     },
     {
       id: 3,
-      name: 'Tipologia T4B',
+      name: 'Tipologia T4A',
       active: false,
     },
     {
       id: 4,
-      name: 'Interior',
+      name: 'Tipologia T4B',
       active: false,
     },
     {
       id: 5,
       name: 'Lazer',
+      active: false,
+    },
+    {
+      id: 6,
+      name: 'Interior',
       active: false,
     },
   ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  send_email_api: 'http://orlando-mailme.fridoom.com/api/'
+  send_email_api: 'https://mailme-app.fridoom.com/api?key=0608050637DA5CD58E2ABFF6',
+  // send_email_api: 'http://orlando-mailme.fridoom.com/api/'
 };
 
 /*
